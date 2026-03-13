9e0ccbe87b5e844d39f1a73dd12fc53e1383dc7a
realrdbr <l.kolberg2010@gmail.com>
2026-03-13 14:13:20 +0100

    Add VITE_PIPER_TTS_URL to vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_PUBLISHABLE_KEY: string;
   readonly VITE_SUPABASE_PROJECT_ID: string;
   readonly VITE_AI_CHAT_URL: string;
+  readonly VITE_PIPER_TTS_URL: string;
 }
 
 interface ImportMeta {
